50cca1cb1b0219ee683806027087f2d3c8b8ffc4
ahmadruslandia <ruslandiaamin@gmail.com>
2023-09-20 00:29:21 +0800

    Basic

---
+++ Kotlin/Contoh Looping/Uncounted Loop/Do While.kt
@@ -0,0 +1,26 @@
+import java.util.Scanner
+
+fun main(args: Array<String>) {
+    var input = Scanner(System.`in`)
+    var ulang = true
+    var i = 0
+    
+    do {
+        println(" ")
+        println("Masukkan huruf y untuk contoh perulangan")
+        println(" ")
+        print("Masukkan huruf : ")
+        var jawab = input.next()
+
+        if( jawab.equals("y") ){
+            ulang = true;
+        }
+        else{
+            ulang = false;
+        }
+        i++;
+    }while (ulang)
+    println(" ")
+    println("Selesai")
+
+}
