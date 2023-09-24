93109f426c24bc04942f33f9fe5a3f9c51f34e27
ahmadruslandia <ruslandiaamin@gmail.com>
2023-09-24 21:41:48 +0800

    Basic

---
+++ Kotlin/Contoh Statement/Contoh If, Else If, dan Else/Nested If dan Else.kt
@@ -0,0 +1,23 @@
+import java.util.Scanner
+
+fun main(args: Array<String>) {
+    var input = Scanner(System.`in`)
+
+    println("Login \n")
+    print("username : ")
+    var data1 = input.nextLine()
+    print("password : ")
+    var data2 = input.nextLine()
+
+    if (data1.equals("user1")){
+        if (data2.equals("psw123")){
+            System.out.println("\nanda berhasil mencoba");
+        }
+        else{
+            System.out.println("\npassword salah");
+        }
+    }
+    else {
+        System.out.println("\niusername anda salah");
+    }
+}
